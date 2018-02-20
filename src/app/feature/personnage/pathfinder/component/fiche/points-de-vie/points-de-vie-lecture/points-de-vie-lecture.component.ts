3675536ebdee28df9e22a4b026cdb754c2bc4111
Orin <architect.orin@gmail.com>
2018-02-20 01:14:56 +0100

    Création du composant points de vies

---
+++ src/app/feature/personnage/pathfinder/component/fiche/points-de-vie/points-de-vie-lecture/points-de-vie-lecture.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'feature-personnage-feuille-points-de-vie-lecture',
+  templateUrl: './points-de-vie-lecture.component.html',
+  styleUrls: ['./points-de-vie-lecture.component.scss']
+})
+export class PointsDeVieLectureComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/feature/personnage/pathfinder/personnage-pathfinder.module.ts
+++ src/app/feature/personnage/pathfinder/personnage-pathfinder.module.ts
@@ -5,7 +5,6 @@ import { SharedModule } from 'app/shared/shared.module';
 import { PersonnagePathfinderRoutingModule } from './personnage-pathfinder-routing.module';
 
 // Controlleurs de pages
-import { PersonnageListePageComponent } from './page/liste/personnage-liste-page.component';
 import { PersonnageEditeurPageComponent } from './page/editeur/personnage-editeur-page.component';
 import { PersonnageFichePageComponent } from './page/fiche/personnage-fiche-page.component';
 
@@ -17,6 +16,7 @@ import { EditeurComponent } from './component/editeur/editeur.component';
 // tslint:disable-next-line:max-line-length
 import { CaracteristiqueLectureComponent } from './component/fiche/caracteristique/caracteristique-lecture/caracteristique-lecture.component';
 import { ResistanceLectureComponent } from './component/fiche/resistance/resistance-lecture/resistance-lecture.component';
+import { PointsDeVieLectureComponent } from './component/fiche/points-de-vie/points-de-vie-lecture/points-de-vie-lecture.component';
 
 
 @NgModule({
@@ -27,13 +27,13 @@ import { ResistanceLectureComponent } from './component/fiche/resistance/resista
   ],
   declarations: [
     PersonnageEditeurPageComponent,
-    PersonnageListePageComponent,
     PersonnageFichePageComponent,
     FicheComponent,
     CiviliteLectureComponent,
     EditeurComponent,
     CaracteristiqueLectureComponent,
-    ResistanceLectureComponent
+    ResistanceLectureComponent,
+    PointsDeVieLectureComponent
   ],
   // Permet d'utiliser une syntaxe <ma-variable> dans les transclusions
   schemas: [NO_ERRORS_SCHEMA]
