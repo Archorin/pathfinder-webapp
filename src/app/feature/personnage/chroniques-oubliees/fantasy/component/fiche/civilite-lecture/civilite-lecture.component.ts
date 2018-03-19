0947a48a35d423bae9fcb3845b87f8c1f56d710b
Orin <architect.orin@gmail.com>
2018-03-19 22:58:32 +0100

    Importation des scss de la maquette V0

--- src/app/feature/personnage/chroniques-oubliees/fantasy/component/fiche/civilite-lecture/civilite-lecture.component.ts
+++ src/app/feature/personnage/chroniques-oubliees/fantasy/component/fiche/civilite-lecture/civilite-lecture.component.ts
@@ -1,12 +1,13 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Personnage } from 'app/feature/personnage/shared/personnage';
 
 @Component({
   selector: 'feature-personnage-cof-fiche-civilite-lecture',
   templateUrl: './civilite-lecture.component.html',
-  styleUrls: ['./civilite-lecture.component.scss']
+  styleUrls: ['./civilite-lecture.component.scss', '../fiche.shared.scss']
 })
 export class CiviliteLectureComponent implements OnInit {
-
+  @Input() personnage: Personnage;
   constructor() { }
 
   ngOnInit() {
