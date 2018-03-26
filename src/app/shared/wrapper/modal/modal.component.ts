21fe2c196fa18551fa4a3e78b7dbf10aa37fb228
Orin <architect.orin@gmail.com>
2018-03-26 17:58:18 +0200

    Création du header COF

--- src/app/shared/wrapper/modal/modal.component.ts
+++ src/app/shared/wrapper/modal/modal.component.ts
@@ -8,20 +8,8 @@ import { ModalDirective } from 'ngx-bootstrap/modal';
 })
 export class ModalComponent {
   @ViewChild(ModalDirective) modal: ModalDirective;
-  messages: string[];
 
   showModal() {
-    console.log('showmodal');
-    this.messages = [];
     this.modal.show();
   }
-
-  handler(type: string, $event: ModalDirective) {
-    this.messages.push(
-      `event ${type} is fired${$event.dismissReason
-        ? ', dismissed by ' + $event.dismissReason
-        : ''}`
-    );
-  }
 }
-
